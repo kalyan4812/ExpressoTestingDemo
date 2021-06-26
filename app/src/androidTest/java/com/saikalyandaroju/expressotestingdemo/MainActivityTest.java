b647f07fec53318e45476fed9dc68bf6d54ceb12
kalyan <darojusaikalyan.civ18@itbhu.ac.in>
2021-06-26 11:26:55 +0530

    Testing Gallery Image Picking

--- app/src/androidTest/java/com/saikalyandaroju/expressotestingdemo/MainActivityTest.java
+++ app/src/androidTest/java/com/saikalyandaroju/expressotestingdemo/MainActivityTest.java
@@ -1,71 +1,68 @@
 package com.saikalyandaroju.expressotestingdemo;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import android.app.Instrumentation;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.provider.MediaStore;
 
+import androidx.activity.result.ActivityResult;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.Activity.RESULT_OK;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
 
-    @Test
-    public void test_isActivityInView() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
 
-        onView(withId(R.id.main)).check(matches(isDisplayed())); // check whether activity is visible.
-    }
+    @Rule
+   public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
-    public void test_visibiltyOfTextAndButton() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
+    public void test_validateGalleryIntent() {
+        //Given
+        Matcher<Intent> expectedIntent = Matchers.allOf(IntentMatchers.hasAction(Intent.ACTION_PICK), IntentMatchers
+                .hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)); // you can have category,extras attahed to itnent.
 
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()));
+        Instrumentation.ActivityResult activityResult = createGalleryPickActivityResultStub();
+        intending(expectedIntent).respondWith(activityResult);
 
-        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()));  //method 1
+        // Execute and Verify
+        onView(withId(R.id.button_open_gallery)).perform(click());
+        intended(expectedIntent);
 
-        onView(withId(R.id.button_next_activity)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))); //method 2.
     }
 
+    private Instrumentation.ActivityResult createGalleryPickActivityResultStub() {
+        Resources resources = InstrumentationRegistry.getInstrumentation().getContext().getResources();
 
-    @Test
-    public void test_isTextDisplayed_Correctly() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)));
+
+        //testing launcher image,becuase it avaialable on each device which we test.
+        Uri imageUri = Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                        resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        );
+        Intent resultIntent = new Intent();
+        resultIntent.setData(imageUri);
+        return new Instrumentation.ActivityResult(RESULT_OK, resultIntent);
     }
-
-    @Test
-    public void test_navigateToSecondaryActivity() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
-        onView(withId(R.id.button_next_activity)).perform(click()); // perform click.
-
-        onView(withId(R.id.secondary)).check(matches(isDisplayed())); // check next activity is displayed.
-    }
-
-    @Test
-    public void test_backPressToMainActivity() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
-        onView(withId(R.id.button_next_activity)).perform(click()); // perform click.
-
-        onView(withId(R.id.secondary)).check(matches(isDisplayed())); // check next activity is displayed.
-
-        // onView(withId(R.id.button_back)).perform(click()); // perform click. method1
-
-        pressBack(); // method 2 back press using expresso.
-
-        onView(withId(R.id.main)).check(matches(isDisplayed()));
-    }
-
-
 }
--- app/src/main/java/com/saikalyandaroju/expressotestingdemo/MainActivity.java
+++ app/src/main/java/com/saikalyandaroju/expressotestingdemo/MainActivity.java
@@ -1,26 +1,54 @@
 package com.saikalyandaroju.expressotestingdemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button next;
+    private static final int GALLERY_REQUEST_CODE = 1234;
+    TextView pickImage;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        next=findViewById(R.id.button_next_activity);
-        next.setOnClickListener(new View.OnClickListener() {
+        pickImage=findViewById(R.id.button_open_gallery);
+        image=findViewById(R.id.image);
+        pickImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,SecondaryActivity.class));
+              pickFromGallery();
             }
         });
+    }
+
+    private void  pickFromGallery() {
+        Intent intent =new  Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode==GALLERY_REQUEST_CODE){
+            Uri uri=data.getData();
+            Glide.with(this)
+                    .load(uri)
+                    .into(image);
+        }
     }
 }
