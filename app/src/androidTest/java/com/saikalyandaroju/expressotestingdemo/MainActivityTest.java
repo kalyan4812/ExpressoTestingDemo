9237290b864d8ba09defad0172535ddb8d36ac8b
kalyan <darojusaikalyan.civ18@itbhu.ac.in>
2021-06-26 12:05:04 +0530

    Testing camera Image Picking

--- app/src/androidTest/java/com/saikalyandaroju/expressotestingdemo/MainActivityTest.java
+++ app/src/androidTest/java/com/saikalyandaroju/expressotestingdemo/MainActivityTest.java
@@ -1,68 +1,69 @@
 package com.saikalyandaroju.expressotestingdemo;
 
+import android.app.Activity;
 import android.app.Instrumentation;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.provider.MediaStore;
 
-import androidx.activity.result.ActivityResult;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.app.Activity.RESULT_OK;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.saikalyandaroju.expressotestingdemo.ImageViewHasDrawableMatcher.hasDrawable;
+import static com.saikalyandaroju.expressotestingdemo.MainActivity.KEY_IMAGE_DATA;
+import static org.hamcrest.Matchers.not;
 
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
 
-
     @Rule
-   public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
-    public void test_validateGalleryIntent() {
-        //Given
-        Matcher<Intent> expectedIntent = Matchers.allOf(IntentMatchers.hasAction(Intent.ACTION_PICK), IntentMatchers
-                .hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)); // you can have category,extras attahed to itnent.
+    public void test_cameraIntent_isBitmapSetToImageView() {
+        // GIVEN
+        Instrumentation.ActivityResult activityResult = createImageCaptureActivityResultStub();
 
-        Instrumentation.ActivityResult activityResult = createGalleryPickActivityResultStub();
+        Matcher<Intent> expectedIntent = Matchers.allOf(IntentMatchers.hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
         intending(expectedIntent).respondWith(activityResult);
 
         // Execute and Verify
-        onView(withId(R.id.button_open_gallery)).perform(click());
+
+      /*  onView(withId(R.id.button_launch_camera)).perform(click());
+        intended(expectedIntent);*/
+
+        onView(withId(R.id.image)).check(matches(not(hasDrawable()))); //before picking there should be no drawable.
+        onView(withId(R.id.button_launch_camera)).perform(click());
         intended(expectedIntent);
+        onView(withId(R.id.image)).check(matches(hasDrawable()));
 
     }
 
-    private Instrumentation.ActivityResult createGalleryPickActivityResultStub() {
-        Resources resources = InstrumentationRegistry.getInstrumentation().getContext().getResources();
-
-
-        //testing launcher image,becuase it avaialable on each device which we test.
-        Uri imageUri = Uri.parse(
-                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
-                        resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
-                        resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
-                        resources.getResourceEntryName(R.drawable.ic_launcher_background)
+    private Instrumentation.ActivityResult createImageCaptureActivityResultStub() {
+        Bundle bundle =new  Bundle();
+        bundle.putParcelable(
+                KEY_IMAGE_DATA, BitmapFactory.decodeResource(
+                        intentsTestRule.getActivity().getResources(),
+                        R.drawable.ic_launcher_background
+                )
         );
-        Intent resultIntent = new Intent();
-        resultIntent.setData(imageUri);
-        return new Instrumentation.ActivityResult(RESULT_OK, resultIntent);
+        Intent resultData = new Intent();
+        resultData.putExtras(bundle);
+        return new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
     }
 }
--- app/src/main/java/com/saikalyandaroju/expressotestingdemo/MainActivity.java
+++ app/src/main/java/com/saikalyandaroju/expressotestingdemo/MainActivity.java
@@ -1,23 +1,22 @@
 package com.saikalyandaroju.expressotestingdemo;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int GALLERY_REQUEST_CODE = 1234;
+
+    private static final int REQUEST_IMAGE_CAPTURE =1234;
+    public static final String KEY_IMAGE_DATA = "data";
+
     TextView pickImage;
     ImageView image;
     @Override
@@ -25,30 +24,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pickImage=findViewById(R.id.button_open_gallery);
+        pickImage=findViewById(R.id.button_launch_camera);
         image=findViewById(R.id.image);
         pickImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              pickFromGallery();
+              dispatchTakePictureIntent();
             }
         });
     }
 
-    private void  pickFromGallery() {
-        Intent intent =new  Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
-
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK && requestCode==GALLERY_REQUEST_CODE){
-            Uri uri=data.getData();
-            Glide.with(this)
-                    .load(uri)
-                    .into(image);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
         }
     }
 }
